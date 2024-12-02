c3f095736c174f9c30fc56cc47156cc7db3bb967
Louis-David Coulombe <louis-david.coulombe@nesto.ca>
2024-12-01 21:34:49 -0500

    feat(tasks): Added copying templates into skeleton

--- tasks.py
+++ tasks.py
@@ -2,6 +2,7 @@ from invoke.tasks import task
 import pathlib as pl
 import datetime
 import aocd
+import shutil
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -21,3 +22,9 @@ def skeleton(c, year=2024, day=None):
     fn = day_folder / "input.txt"
     with open(fn, "w", encoding="utf-8") as f:
         f.write(aocd.get_data(day=int(day), year=year))
+
+    for fn in ["./templates/main.py", "./templates/test_challenge.py"]:
+        tmpl = pl.Path(fn)
+        shutil.copy2(tmpl, day_folder / tmpl.name)
+
+    
