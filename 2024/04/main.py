5a9dd8e679ec0fddf62a119c63205ce0dda77989
Louis-David Coulombe <louis-david.coulombe@nesto.ca>
2024-12-06 21:14:59 -0500

    Added 04/05 skeleton

---
+++ 2024/04/main.py
@@ -0,0 +1,37 @@
+import pathlib
+from argparse import ArgumentParser
+import pyperclip
+
+
+def parse_input(txt: str):
+    left, right = [], []
+    for line in txt.split("\n"):
+        a, b = line.split()
+        left.append(int(a))
+        right.append(int(b))
+    return left, right
+
+
+def part1(txt: str) -> int:
+    print(txt)
+    return 0
+
+
+def part2(txt: str) -> int:
+    print(txt)
+    return 0 
+
+
+def main(args):
+    function = [part1, part2]
+    current_dir = pathlib.Path(__file__).parent.resolve()
+    with open(current_dir / "input.txt", "r", encoding="utf-8") as f:
+        result = function[args.p](f.read())
+        pyperclip.copy(result)
+        print(result)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--p", type=int, default=0)
+    main(parser.parse_args())
