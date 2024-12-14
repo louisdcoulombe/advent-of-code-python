af6e4eaa7a7066d0ecfa0c785ead482595c9c43a
Louis-David Coulombe <louis-david.coulombe@nesto.ca>
2024-12-14 11:01:01 -0500

    feat(2024/07): skeleton

---
+++ 2024/07/test_challenge.py
@@ -0,0 +1,23 @@
+import pytest
+from main import parse_input, part1, part2
+
+EXAMPLE = """
+
+""".strip()
+
+def test_parse():
+    a, b = parse_input(EXAMPLE)
+    assert a == [3, 4, 2, 1, 3, 3]
+    assert b == [4, 3, 5, 3, 9, 3]
+
+
+def test_part1():
+    result = part1(EXAMPLE)
+    assert result == 11
+
+def test_part2():
+    result = part2(EXAMPLE)
+    assert result == 31
+
+if __name__ == '__main__':
+    pytest.main(['-vv'])
